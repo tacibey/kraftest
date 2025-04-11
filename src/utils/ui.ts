6e61ccfd785a9ce018189b7b77ac283bf2ffd0f1
Taci <taciyalcin@gmail.com>
2025-04-11 12:37:08 +0300

    Update ui.ts

--- src/utils/ui.ts
+++ src/utils/ui.ts
@@ -1,4 +1,10 @@
+// src/utils/ui.ts
 export const languages = {
-    en: "English",
-    fr: "Français",
+  en: "English",
+  fr: "Français", 
+  es: "Español",
+  ru: "Русский",
+  de: "Deutsch",
+  pt: "Português",
+  it: "Italiano",
 };
